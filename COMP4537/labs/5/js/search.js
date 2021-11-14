03b3fee40a115ad3acd4f14b910290ce5eb733c8
GoodestUsername <ericdongcannn@hotmail.com>
2021-11-13 16:25:58 -0800

    added  index.html, and set readdb to interval of 2 seconds

--- COMP4537/labs/5/js/search.js
+++ COMP4537/labs/5/js/search.js
@@ -11,18 +11,17 @@ function InfoBlock(info) {
     this.div.appendChild(this.p);
     document.getElementById("table").appendChild(this.div);
 }
+setInterval(function () {
+    xhttp.open("GET", url, true);
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            let response = this.responseText;
+            let responseOBJ = JSON.parse(response);
 
-xhttp.open("GET", url, true);
-xhttp.send();
-xhttp.onreadystatechange = function() {
-    if(this.readyState == 4 && this.status == 200) {
-        let response = this.responseText;
-        let responseOBJ = JSON.parse(response);
-
-        responseOBJ.forEach(element => {
-            InfoBlock(element);
-        });
-        console.log(response);
-        console.log(responseOBJ);
-    }
-};
+            responseOBJ.forEach(element => {
+                InfoBlock(element);
+            });
+        }
+    };
+}, 2000);
